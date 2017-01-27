e1f943c36e744a2e06acb5ca4f071d9e7b646715
Ryan Barron <ryanbarron243@gmail.com>
2017-01-27 13:46:16 -0500

    finished assignment

--- main.js
+++ main.js
@@ -1,26 +1,65 @@
-// grab all the prices of the items in the list. and calculate the average. easy enough right? WRONG its HangerDesignCenter
+// // grab all the prices of the items in the list. and calculate the average. easy enough right? WRONG its HangerDesignCenter
 // var sum = items.reduce(function(total, current) {
 //   return total + current.price;
 // },0);
 //
 // console.log("The average price is " + (sum / items.length).toFixed(2));
-
+//
+//
+// /*
+// *  number 2
+// */
+// var newArray = items.filter(function(current){
+//   return current.price > 14 && current.price < 18;
+// });
+//
+// console.log(newArray);
+//
+// /*
+// * number 3
+// */
+//  // use forEach
+// var itemArray = items.filter(function(current) {
+//   return current.currency_code === 'GBP';
+// })
+// itemArray.forEach(function(item){
+//   console.log(item.title + " " + item.price);
+// })
+// // console.log(itemArray);
+//
+//
+// /*
+// * Number FOUR!!!
+// */
+// var woodArray = items.filter(function(item){
+//   if(item.materials.indexOf("wood") !== -1){
+//    console.log(item.materials);
+//    console.log(item.title + " is made of wood");
+//   }
+// })
+// console.log(woodArray);
 
 /*
-*  number 2
+* Number FIVE!!!
 */
-var newArray = items.filter(function(current){
-  return current.price > 14 && current.price < 18;
-  console.log(newArray);
 
+var materialsGreaterThanEight = items.filter(function(item){
+  return item.materials.length > 7;
 })
-console.log(newArray);
+
+materialsGreaterThanEight.forEach(function(item){
+  console.log(item.title);
+for(var i = 0; i > 7; i++){
+  console.log(i);
+}
+})
+console.log(materialsGreaterThanEight);
+
 
 /*
-* number 3
+*Number six!!!
 */
- // use forEach
-var itemArray = items.forEach(function(current) {
-  return current.price > 20;
-  console.log(itemArray);
+
+var itemsSold = items.filter(function(item){
+  console.log(item.quantity + " were made by their sellers");
 })
