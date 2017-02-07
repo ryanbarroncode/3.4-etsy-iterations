95396eba4326cb0b1c56bfcf8ada8ae335c070f8
Ryan Barron <ryanbarron243@gmail.com>
2017-02-06 20:09:35 -0500

    final touches

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
-// // grab all the prices of the items in the list. and calculate the average. easy enough right? WRONG its HangerDesignCenter
-// var sum = items.reduce(function(total, current) {
-//   return total + current.price;
-// },0);
-//
-// console.log("The average price is " + (sum / items.length).toFixed(2));
-//
-//
-// /*
-// *  number 2
-// */
-// var newArray = items.filter(function(current){
-//   return current.price > 14 && current.price < 18;
-// });
-//
-// console.log(newArray);
-//
-// /*
-// * number 3
-// */
-//  // use forEach
-// var itemArray = items.filter(function(current) {
-//   return current.currency_code === 'GBP';
-// })
-// itemArray.forEach(function(item){
-//   console.log(item.title + " " + item.price);
-// })
-// // console.log(itemArray);
-//
-//
-// /*
-// * Number FOUR!!!
-// */
-// var woodArray = items.filter(function(item){
-//   if(item.materials.indexOf("wood") !== -1){
-//    console.log(item.materials);
-//    console.log(item.title + " is made of wood");
-//   }
-// })
-// console.log(woodArray);
+// grab all the prices of the items in the list. and calculate the average. easy enough right? WRONG its HangerDesignCenter
+var sum = items.reduce(function(total, current) {
+  return total + current.price;
+},0);
+
+console.log("The average price is " + (sum / items.length).toFixed(2));
+
+
+/*
+*  number 2
+*/
+var newArray = items.filter(function(current){
+  return current.price > 14 && current.price < 18;
+});
+
+console.log(newArray);
+
+/*
+* number 3
+*/
+ // use forEach
+var itemArray = items.filter(function(current) {
+  return current.currency_code === 'GBP';
+})
+itemArray.forEach(function(item){
+  console.log(item.title + " " + item.price);
+})
+// console.log(itemArray);
+
+
+/*
+* Number FOUR!!!
+*/
+var woodArray = items.filter(function(item){
+  if(item.materials.indexOf("wood") !== -1){
+   console.log(item.materials);
+   console.log(item.title + " is made of wood");
+  }
+})
+console.log(woodArray);
 
 /*
 * Number FIVE!!!
